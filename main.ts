cdeaca13df264c8f6f62ab7d9032b2303834d6d6
sarab42 <97531187+sarab42@users.noreply.github.com>
2022-02-01 13:41:45 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
-input.onPinPressed(TouchPin.P0, function () {
-	
-})
 basic.forever(function () {
-	
+    if (input.pinIsPressed(TouchPin.P0)) {
+        music.playTone(494, music.beat(BeatFraction.Whole))
+        basic.showIcon(IconNames.Heart)
+    } else if (input.pinIsPressed(TouchPin.P1)) {
+        music.playTone(440, music.beat(BeatFraction.Whole))
+        basic.showIcon(IconNames.Duck)
+    } else if (input.pinIsPressed(TouchPin.P2)) {
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        basic.showIcon(IconNames.Umbrella)
+    } else {
+    	
+    }
 })
